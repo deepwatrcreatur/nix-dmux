de5a73d1de65b08c87fc759286796151a364e98c
Anwer Khan <deepwatrcreatur@gmail.com>
2026-03-13 08:25:42 -0400

    feat: Add Darwin packages to nix-dmux flake

--- flake.nix
+++ flake.nix
@@ -8,6 +8,8 @@
   outputs = { self, nixpkgs }: {
     packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.callPackage ./pkgs { };
     packages.aarch64-linux.default = nixpkgs.legacyPackages.aarch64-linux.callPackage ./pkgs { };
+    packages.x86_64-darwin.default = nixpkgs.legacyPackages.x86_64-darwin.callPackage ./pkgs { }; # Added for Intel Macs
+    packages.aarch64-darwin.default = nixpkgs.legacyPackages.aarch64-darwin.callPackage ./pkgs { }; # Added for Apple Silicon Macs
     # Add other systems if needed
 
     # You can also define overlays here if you want dmux to be part of an overlay
