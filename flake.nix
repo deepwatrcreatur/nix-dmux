b5ba0ac83bf676c03a6263c5bbc9713dd71c0818
Anwer Khan <deepwatrcreatur@gmail.com>
2026-03-30 09:50:45 -0400

    chore: automate package updates

--- flake.nix
+++ flake.nix
@@ -5,16 +5,26 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Use your preferred Nixpkgs branch
   };
 
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.callPackage ./pkgs { };
-    packages.aarch64-linux.default = nixpkgs.legacyPackages.aarch64-linux.callPackage ./pkgs { };
-    packages.x86_64-darwin.default = nixpkgs.legacyPackages.x86_64-darwin.callPackage ./pkgs { }; # Added for Intel Macs
-    packages.aarch64-darwin.default = nixpkgs.legacyPackages.aarch64-darwin.callPackage ./pkgs { }; # Added for Apple Silicon Macs
-    # Add other systems if needed
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = self.packages.${system}.dmux;
+          dmux = pkgs.callPackage ./pkgs { };
+        }
+      );
 
-    # You can also define overlays here if you want dmux to be part of an overlay
-    # overlays.default = final: prev: {
-    #   dmux = final.callPackage ./pkgs { };
-    # };
-  };
+      overlays.default = final: prev: {
+        dmux = self.packages.${prev.system}.dmux;
+      };
+    };
 }
